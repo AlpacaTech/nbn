355c8539879ae651dae1a80b43a9819658512082
iuyte <iuyte@gmx.com>
2017-07-25 01:41:54 -0400

    general work

--- game.go
+++ game.go
@@ -1,11 +1,66 @@
 package main
 
 import (
-	// "github.com/faiface/pixel"
+	"image"
+	"os"
+
+	_ "image/png"
+
+	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
-	"golang.org/x/image/colornames"
 )
 
-func Update(win *pixelgl.Window) {
-	win.Clear(colornames.Cornflowerblue)
+type Game struct {
+	Title    string
+	Win      *pixelgl.Window
+	Pictures []pixel.Picture
+}
+
+func NewGame(title string, bounds pixel.Rect, resizable bool) *Game {
+	cfg := pixelgl.WindowConfig{
+		Title:     title,
+		Bounds:    bounds,
+		Resizable: resizable,
+		VSync:     true,
+	}
+
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	return &Game{
+		Title: title,
+		Win:   win,
+	}
+}
+
+func (g *Game) LoadPicture(path string) (int, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return 0, err
+	}
+	g.Pictures = append(g.Pictures, pixel.PictureDataFromImage(img))
+	return len(g.Pictures) - 1, nil
+}
+
+/*
+func (g *Game) TransitionWindowBounds(bounds pixel.Rect) {
+	b := g.Win.Bounds()
+	an int = (math.Abs(b.Max.X - bounds.Max.X) + math.Abs(b.Max.Y - Bounds.Max.Y)) / 2
+	for g.Win.Bounds().Size().X {}
+}
+*/
+
+func (g *Game) Open() bool {
+	return !g.Win.Closed()
+}
+
+func (g *Game) Loop() {
+	g.Win.Update()
 }
--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"time"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
+var (
+	game    *Game
+	gophers []*pixel.Sprite
 )
 
 func main() {
@@ -10,20 +18,43 @@ func main() {
 }
 
 func run() {
-	cfg := pixelgl.WindowConfig{
-		Title:     "Pixel Rocks!",
-		Bounds:    pixel.R(0, 0, 1280, 768),
-		Resizable: true,
-		VSync:     true,
-	}
+	game = NewGame("Stay Alive", pixel.R(0, 0, 1280, 768), false)
+	center := game.Win.Bounds().Center()
 
-	win, err := pixelgl.NewWindow(cfg)
+	loadGophers()
+
+	animate := time.Now().Add(time.Millisecond * 250)
+	for blink := false; game.Open(); {
+		game.Loop()
+		game.Win.Clear(colornames.Cornflowerblue)
+
+		pos := pixel.IM.Moved(center).Scaled(center, 5)
+		if blink {
+			gophers[1].Draw(game.Win, pos)
+
+			if time.Now().After(animate) {
+				blink = !blink
+				animate = time.Now().Add(time.Millisecond * 250)
+			}
+		} else {
+			gophers[0].Draw(game.Win, pos)
+
+			if time.Now().After(animate) {
+				blink = !blink
+				animate = time.Now().Add(time.Millisecond * 50)
+			}
+		}
+	}
+}
+
+func loadGophers() {
+	spritesheet, err := game.LoadPicture("./img/gopher.png")
 	if err != nil {
 		panic(err)
 	}
 
-	for !win.Closed() {
-		Update(win)
-		win.Update()
+	var l, h float64 = 12, 14
+	for i := 0.0; i < 27; i++ {
+		gophers = append(gophers, pixel.NewSprite(game.Pictures[spritesheet], pixel.R(12*i, 0, 12*i+l, h)))
 	}
 }
